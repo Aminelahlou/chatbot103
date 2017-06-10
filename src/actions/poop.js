38485716e857c2fd91cb381eba63bad0ab4c3729
Anselme Grumbach <agrumbac@student.42.fr>
2017-06-10 20:11:40 +0200

    [FIX--WIP] poop v2

--- src/actions/poop.js
+++ src/actions/poop.js
@@ -5,7 +5,6 @@ export default async function showLanguage(res) {
   console.log('POOP')
 
   const replies = []
-  const quickReplies = []
   replies.push(formatter.formatImg("http://2.bp.blogspot.com/-JyYlr_9zvXc/Tg24irCIz2I/AAAAAAAACNY/m-8H2D43q4k/s1600/parenting-fail-elephant-poop.jpg"))
   return replies
 }
