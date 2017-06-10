9d2067903a586e16ea250109fd0a16ddf22b50d4
Anselme Grumbach <agrumbac@student.42.fr>
2017-06-10 20:03:11 +0200

    [ADD] poop

---
+++ src/actions/poop.js
@@ -0,0 +1,11 @@
+const agent = require('superagent-promise')(require('superagent'), Promise)
+const formatter = require('../formatter')
+
+export default async function showLanguage(res) {
+  console.log('POOP')
+
+  const replies = []
+  const quickReplies = []
+  replies.push(formatter.formatImg("http://2.bp.blogspot.com/-JyYlr_9zvXc/Tg24irCIz2I/AAAAAAAACNY/m-8H2D43q4k/s1600/parenting-fail-elephant-poop.jpg"))
+  return replies
+}
